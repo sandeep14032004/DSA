feb6bc03d67fcce3c7b5ca917490eec91f5c4539
sandeep14032004 <sandeeppati69@gmail.com>
2024-08-02 10:52:45 +0530

    array initialization and traverse

---
+++ Arrays/basic.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+import java.util.*;
+
+public class basic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How many number to store in the array : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("The array is : ");
+        // for (int i = 0; i < arr.length; i++) {
+        // System.out.print(arr[i] + "\t");
+        // }
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}

b549373698e5f754a891782da57965dd5f9830b0
sandeep14032004 <sandeeppati69@gmail.com>
2024-08-02 20:07:02 +0530

    deletion

---
+++ Arrays/deletion.java
@@ -0,0 +1,38 @@
+package Arrays;
+
+public class deletion {
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
+        int value = 3;
+        int newArray[] = deleteValue(arr, value);
+        for (int i : newArray) {
+            System.err.println(i);
+        }
+    }
+
+    public static int[] deleteValue(int[] arr, int value) {
+        int pos = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == value) {
+                pos = i;
+                break;
+            }
+        }
+        if (pos == -1)
+            return arr;
+        return deletion(arr, pos);
+    }
+
+    public static int[] deletion(int arr[], int pos) {
+        int[] newArray = new int[arr.length - 1];
+        for (int i = 0; i < pos; i++) {
+            newArray[i] = arr[i];
+
+        }
+        for (int i = pos + 1; i < arr.length; i++) {
+            newArray[i - 1] = arr[i];
+
+        }
+        return newArray;
+    }
+}
---
+++ Arrays/insertionLast.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+public class insertionLast {
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 5 };
+        int value = 12;
+        int newArray[] = insertionLast(arr, value);
+        for (int i : newArray) {
+            System.out.println(i);
+        }
+
+    }
+
+    public static int[] insertionLast(int arr[], int value) {
+        int[] newArray = new int[arr.length + 1];
+        for (int i = 0; i < arr.length; i++) {
+            newArray[i] = arr[i];
+        }
+        newArray[arr.length] = value;
+        return newArray;
+    }
+
+}

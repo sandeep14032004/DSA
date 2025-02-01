3b54a7633cff84b0ec1d5b721de4cd278630e09a
sandeep14032004 <sandeeppati69@gmail.com>
2025-02-01 19:56:57 +0530

    majority element

---
+++ Leetcode/169.majority-element.java
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=169 lang=java
+ *
+ * [169] Majority Element
+ */
+
+// @lc code=start
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int majorityElement = nums[0], votes = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (votes == 0) {
+                votes++;
+                majorityElement = nums[i];
+            } else if (majorityElement == nums[i]) {
+                votes++;
+            } else {
+                votes--;
+            }
+        }
+        return majorityElement;
+    }
+}
+
+// @lc code=end
+// int n = nums.length;
+// int halfn = n / 2;for(
+// int i = 0;i<n;i++)
+// {
+// int count = 0;
+// for (int j = 0; j < n; j++) {
+// if (nums[i] == nums[j]) {
+// count++;
+// }
+// }
+// if (count > halfn) {
+// return nums[i];
+// }
+// }return-1
+// ;

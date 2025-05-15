b23724e5f0b8e90c0772269cbac2008977375e4f
sandeep14032004 <sandeeppati69@gmail.com>
2025-05-15 19:47:51 +0530

    Extraction of number

---
+++ SDSA/basic_maths/ExtractionOfNumbers.java
@@ -0,0 +1,11 @@
+public class ExtractionOfNumbers {
+
+    public static void main(String[] args) {
+        int n = 1223;
+        while (n != 0) {
+            int remainder = n % 10;
+            n = n / 10;
+            System.out.print(remainder);
+        }
+    }
+}

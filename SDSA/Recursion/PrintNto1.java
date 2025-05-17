e80dd2b0912a2022a4c08ccdcfebcd8c6b0ed456
sandeep14032004 <sandeeppati69@gmail.com>
2025-05-17 11:44:13 +0530

    Printing N to 1

---
+++ SDSA/Recursion/PrintNto1.java
@@ -0,0 +1,14 @@
+public class PrintNto1 {
+
+    public static void Print(int n) {
+        if (n == 0) {
+            return;
+        }
+        System.out.println(n + " ");
+        Print(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Print(5);
+    }
+}

2c8c731ac098d3ef50367236382a8a6c820cc18f
sandeep14032004 <sandeeppati69@gmail.com>
2025-05-29 12:51:22 +0530

    left rotate by once bruteforce

---
+++ SDSA/arrays/LeftRotateBruteForce.java
@@ -0,0 +1,20 @@
+public class LeftRotateBruteForce {
+    public static int[] rotateArr(int arr[]) {
+        int first = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            arr[i - 1] = arr[i];
+        }
+
+        arr[arr.length - 1] = first;
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4 };
+        for (int i : rotateArr(arr)) {
+            System.out.print(i + " ");
+        }
+    }
+}

a5b28adfb354ed9577300e7ab3bf58a499f436f4
sandeep14032004 <sandeeppati69@gmail.com>
2025-06-04 21:11:55 +0530

    buy and sell stock bruteforce

---
+++ SDSA/arrays/StockBuyAndSell.java
@@ -0,0 +1,20 @@
+public class StockBuyAndSell {
+    public static int Mareket(int arr[]) {
+        int n = arr.length;
+        int profit = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                if (arr[j] - arr[i] > profit) {
+                    profit = arr[j] - arr[i];
+                }
+            }
+        }
+        return profit;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 7, 1, 5, 3, 6, 4 };
+        int res = Mareket(arr);
+        System.out.println(res);
+    }
+}
